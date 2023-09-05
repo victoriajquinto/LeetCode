a5ef5c9e98231bed62ed6e45ef3c7a4050f060a9
Vicky Quinto <victoriajquinto@gmail.com>
2023-09-05 14:24:06 -0700

    Time: 140 ms (93.51%), Space: 59.2 MB (47.5%) - LeetHub

---
+++ 0015-3sum/0015-3sum.js
@@ -0,0 +1,46 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+ //i: array of numbers
+ //o: an array of nested arrays, each containing 3 unique elements that sum to 0 
+ //c: may contain negative numbers, input array length between 3 and 3000 inclusive
+ //e: no possible solution -> return empty array
+ //if a+b+c = 0, then a = b+c, where a is a given element
+ //at a given element, find two other elements sum to that element
+    //if found, add all three elements to results array
+    //otherwise, continue to the next element
+var threeSum = function(nums) {
+    //declare results array
+    let results = [];
+    //sort nums asc
+    nums.sort((a,b) => {return a-b});
+    //loop through nums array
+    for(let i = 0; i < nums.length; i++) {
+        //assign a to number at incrementer
+        let a = nums[i];
+        //if a is greater than 0, break
+        if (a > 0) break;
+        // if (a === previous) continue;
+        if (i > 0 && a === nums[i-1]) continue;
+        let [ left, right ] = [i+1, nums.length-1];
+        //while left < right
+        while (left < right) {
+            let tripletSum = a + nums[left] + nums[right];
+            if (tripletSum < 0) {
+                left++;
+            } else if ( tripletSum > 0) {
+                right--;
+            //if a === b + c, push [a,b,c] into results array
+            } else {
+                results.push([a, nums[left], nums[right]]);
+                left++;
+                right--;
+                while (nums[left] === nums[left-1] && left < right){
+                    left++;
+                } 
+            }
+        }
+    }
+    return results;
+};
