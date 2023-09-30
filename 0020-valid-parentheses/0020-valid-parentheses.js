8f9dc7c609e3f728e5e6a71719c14e016fa1d561
Vicky Quinto <victoriajquinto@gmail.com>
2023-09-29 20:40:11 -0700

    Time: 49 ms (86.91%), Space: 42.6 MB (37.99%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -0,0 +1,37 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    //create a stack array
+    const stack = [];
+    //create a reference object with bracket pairs
+    const reference = {
+        "}": "{",
+        ")": "(",
+        "]": "[",
+    };
+
+    //convert s to array
+    s = s.split('');
+    //loop through s
+    for(let i = 0; i < s.length; i++) {
+        let char = s[i];
+        //if char = (, {, or [,
+        if (char === '(' || char ==='{' || char ==='[') {
+            //push char into stack
+            stack.push(char);
+        } else if (stack[stack.length - 1] === reference[char]) {
+            if(stack.length === 0) {
+                return false;
+            }
+            //pop last char from stack
+            stack.pop();
+        } else {
+            return false;
+        };
+    }
+    //if stack is empty and s is empty, return true, otherwise, return false
+        return (stack.length === 0) ? true : false;
+};
+
